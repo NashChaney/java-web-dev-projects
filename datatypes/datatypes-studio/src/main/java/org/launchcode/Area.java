2e0a7ca770875d75bbbef53dc20f77533c422451
Nash Chaney <nchaney0508@gmail.com>
2024-04-01 20:11:20 -0500

    Completed Studio 1

---
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -0,0 +1,14 @@
+package org.launchcode;
+
+import java.util.Scanner;
+
+public class Area {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a radius: ");
+        double radius = input.nextDouble();
+        double area = Circle.getArea(radius);
+        System.out.println("The area of the circle is: " + area);
+    }
+}
